f68df554f7d9cd933aa12e6c12f6ffc733908f2d
Mago <mago0025@gmail.com>
2021-10-11 00:01:26 -0500

    integracion con el backend

---
+++ src/utils/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const obtenerProductos = async (setProductos, setEjecutarConsulta) => {
+  const options = { method: 'GET', url: 'http://localhost:8000/Productos' };
+  await axios
+    .request(options)
+    .then(function (response) {
+      setProductos(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  setEjecutarConsulta(false);
+};
