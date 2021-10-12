eb553f02f63a5e2538d4761ec63ccc8e4d5eaef7
fdo <fernandovillaquiran@outlook.com>
2021-10-12 06:48:21 -0500

    Agregue en api.js en utils la funcion get de Usuarios

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,27 @@
-import axios from 'axios';
+import axios from 'axios'
 
 export const obtenerProductos = async (setProductos, setEjecutarConsulta) => {
-  const options = { method: 'GET', url: 'http://localhost:8000/Productos' };
+  const options = { method: 'GET', url: 'http://localhost:8000/Productos' }
   await axios
     .request(options)
     .then(function (response) {
-      setProductos(response.data);
+      setProductos(response.data)
     })
     .catch(function (error) {
-      console.error(error);
-    });
-  setEjecutarConsulta(false);
-};
+      console.error(error)
+    })
+  setEjecutarConsulta(false)
+}
+
+export const obtenerUsuarios = async (setUsuarios, setEjecutarConsulta) => {
+  const options = { method: 'GET', url: 'http://localhost:8000/Usuarios' }
+  await axios
+    .request(options)
+    .then(function (response) {
+      setUsarios(response.data)
+    })
+    .catch(function (error) {
+      console.error(error)
+    })
+  setEjecutarConsulta(false)
+}
